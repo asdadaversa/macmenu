ee75c50ae2f100f12722da185c7d95e01946c449
asdadaversa <peikqudakeus@gmail.com>
2024-04-09 18:32:37 +0300

    create endpoint for detail product view and detail/field - view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,3 @@
-import datetime
-
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -10,7 +8,7 @@ from products.serializers import ProductsSerializer
 
 
 class ProductsListView(APIView):
-    """ APIVIEW for lista """
+    """ APIVIEW for list products """
 
     serializer_class = ProductsSerializer
 
@@ -18,3 +16,31 @@ class ProductsListView(APIView):
         products = Products.objects.all()
         serializer = ProductsSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProductsDetailView(APIView):
+    """ APIVIEW for detail products by latinos_name """
+
+    def get(self, request, latinos_name, field_name=None, format=None):
+        product = get_object_or_404(Products, latinos_name=latinos_name)
+        serializer = ProductsSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProductsDetailFieldView(APIView):
+    """ APIVIEW for detail products by latinos name and fields """
+
+    def get(self, request, latinos_name, field_name=None, format=None):
+        product = get_object_or_404(Products, latinos_name=latinos_name)
+        if field_name:
+            try:
+                field_value = getattr(product, field_name)
+                return Response({field_name: field_value}, status=status.HTTP_200_OK)
+            except AttributeError:
+                return Response(
+                    {"error": f"Field '{field_name}' does not exist"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        else:
+            serializer = ProductsSerializer(product)
+            return Response(serializer.data, status=status.HTTP_200_OK)
