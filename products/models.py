49123caa302ed756eeb772009c061683bfa8b30d
asdadaversa <peikqudakeus@gmail.com>
2024-04-09 11:20:05 +0300

    create products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    latinos_name = models.CharField(max_length=30, null=False, unique=True)
+    name = models.CharField(max_length=30, null=False, unique=True)
+    description = models.TextField(null=True)
+    calories = models.CharField(max_length=63)
+    fats = models.CharField(max_length=63)
+    carbs = models.CharField(max_length=63)
+    proteins = models.CharField(max_length=63)
+    unsaturated_fats = models.CharField(max_length=63)
+    sugar = models.CharField(max_length=63)
+    salt = models.CharField(max_length=63)
+    portion = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.name}"
